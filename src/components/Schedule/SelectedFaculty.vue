d4b26961e27a8b0800df5fc4d192863863929c9a
Nikita <nikita.vorontsov.1989@mail.ru>
2025-04-22 14:23:22 +0300

    Пытаюсь сделать выбор факультете, формы обучения, и тд

---
+++ src/components/Schedule/SelectedFaculty.vue
@@ -0,0 +1,24 @@
+<template>
+    <select v-model="selectedFaculty" @change="$emit('select-faculty', selectedFaculty)"
+        class="border rounded-lg p-4 bg-white text-center">
+        <option selected disabled hidden :value="null">Факультет</option>
+        <option v-for="faculty in arrFaculty" :key="faculty.name" :value="faculty"
+            class="border rounded-lg p-4 bg-white text-center">
+            {{ faculty.name }}
+        </option>
+    </select>
+
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+import { useFormFaculty } from "@/stores/getFormFaculty";
+import { ref } from "vue";
+
+const selectedFaculty = ref(null);
+
+const formFacultyStore = useFormFaculty();
+const { arrFaculty } = storeToRefs(formFacultyStore);
+
+defineEmits(["select-faculty"]);
+</script>
