8af86ab8734b9b41e59e05e1767dbb21855c8198
Nikita <nikita.vorontsov.1989@mail.ru>
2025-04-22 14:24:25 +0300

    Изменения

---
+++ src/components/Schedule/SelectedForm.vue
@@ -0,0 +1,23 @@
+<template>
+    <select v-model="selectedForm" @change="$emit('select-form', selectedForm)"
+        class="border rounded-lg p-4 bg-white text-center">
+        <option selected disabled hidden :value="null">Форма обучения</option>
+        <option v-for="form in arrForm" :key="form.name" :value="form"
+            class="border rounded-lg p-4 bg-white text-center">
+            {{ form.name }}
+        </option>
+    </select>
+</template>
+
+<script setup>
+import { storeToRefs } from "pinia";
+import { useFormFaculty } from "@/stores/getFormFaculty";
+import { ref } from "vue";
+
+const selectedForm = ref(null);
+
+const formFacultyStore = useFormFaculty();
+const { arrForm } = storeToRefs(formFacultyStore);
+
+defineEmits(["select-form"]);
+</script>
