7cd786183d389bd6376e939ddf0ab71962529b6e
Nikita <nikita.vorontsov.1989@mail.ru>
2025-04-30 23:42:40 +0300

    Перемечтил script в верхнюю часть файла, тк об этом договорилось сообщество по Vue, что script будет находиться сверху, так же форматировал файлы

--- src/components/Auditoriums/BuildingSelector.vue
+++ src/components/Auditoriums/BuildingSelector.vue
@@ -1,35 +1,35 @@
-<template>
-    <div class="grid  grid-cols-1 md:grid-cols-3 gap-4 p-4 bg-gray-50 rounded-lg">
-      <div 
-        v-for="building in buildings" 
-        :key="building.type" 
-        class="border dark:bg-[#2f2f2f] dark:text-white rounded-lg p-4 bg-white text-center"
-      >
-        <h3 class="font-medium mb-2">{{ building.label }}</h3>
-        <AppButton
-          @click="$emit('select-building', building.type)"
-          :disabled="isLoading"
-        >
-          Получить аудитории
-        </AppButton>
-      </div>
-    </div>
-  </template>
-  
-  <script setup>
-import AppButton from '../UI/AppButton.vue';
+<script setup>
+import AppButton from "../UI/AppButton.vue";
 
-  defineProps({
-    isLoading: Boolean,
-    buildings: {
-      type: Array,
-      default: () => [
-        { type: 'auditoriumsInNewCorpus', label: 'Новый корпус' },
-        { type: 'auditoriumsInOldCorpus', label: 'Старый корпус' },
-        { type: 'auditoriumsInDormitory', label: 'Общага' }
-      ]
-    }
-  })
-  
-  defineEmits(['select-building'])
-  </script>
+defineProps({
+  isLoading: Boolean,
+  buildings: {
+    type: Array,
+    default: () => [
+      { type: "auditoriumsInNewCorpus", label: "Новый корпус" },
+      { type: "auditoriumsInOldCorpus", label: "Старый корпус" },
+      { type: "auditoriumsInDormitory", label: "Общага" },
+    ],
+  },
+});
+
+defineEmits(["select-building"]);
+</script>
+
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 bg-gray-50 rounded-lg">
+    <div
+      v-for="building in buildings"
+      :key="building.type"
+      class="border dark:bg-[#2f2f2f] dark:text-white rounded-lg p-4 bg-white text-center"
+    >
+      <h3 class="font-medium mb-2">{{ building.label }}</h3>
+      <AppButton
+        @click="$emit('select-building', building.type)"
+        :disabled="isLoading"
+      >
+        Получить аудитории
+      </AppButton>
+    </div>
+  </div>
+</template>
--- src/components/Auditoriums/ControlPanel.vue
+++ src/components/Auditoriums/ControlPanel.vue
@@ -1,30 +1,29 @@
+<script setup>
+import AppButton from "../UI/AppButton.vue";
+
+defineProps({
+  isLoading: Boolean,
+  actions: {
+    type: Array,
+    default: () => [
+      { label: "Инициализация аудиторий", method: "init" },
+      { label: "Очистка Local Storage", method: "clear" },
+    ],
+  },
+});
+
+defineEmits(["action"]);
+</script>
+
 <template>
-    <div class="flex flex-wrap gap-3  p-4 bg-gray-50 rounded-lg">
-      <AppButton
-        v-for="(action, index) in actions"
-        :key="index"
-        @click="$emit('action', action.method)"
-        :disabled="isLoading"
-      >
-        {{ action.label }}
-      </AppButton>
-
-    </div>
-  </template>
-  
-  <script setup>
-import AppButton from '../UI/AppButton.vue';
-
-  defineProps({
-    isLoading: Boolean,
-    actions: {
-      type: Array,
-      default: () => [
-        { label: 'Инициализация аудиторий', method: 'init' }, 
-        { label: 'Очистка Local Storage', method: 'clear' }
-      ]
-    }
-  })
-  
-  defineEmits(['action'])
-  </script>
+  <div class="flex flex-wrap gap-3 p-4 bg-gray-50 rounded-lg">
+    <AppButton
+      v-for="(action, index) in actions"
+      :key="index"
+      @click="$emit('action', action.method)"
+      :disabled="isLoading"
+    >
+      {{ action.label }}
+    </AppButton>
+  </div>
+</template>
