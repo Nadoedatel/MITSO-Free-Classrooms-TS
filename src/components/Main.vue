dd4c694d8bceadf41aff9af8f7954d4875428a6a
Nikita <nikita.vorontsov.1989@mail.ru>
2025-04-30 23:49:27 +0300

    Добавил явную типизацию lang="ts" и переписал под ts

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import AppButton from "./UI/AppButton.vue";
 import SwitchTheme from "./UI/SwitchTheme.vue";
--- src/components/UI/SwitchTheme.vue
+++ src/components/UI/SwitchTheme.vue
@@ -1,23 +1,23 @@
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from "vue";
 
-const isDark = ref(false);
+const isDark = ref<boolean>(false);
 
 onMounted(() => {
-  const savedMode = localStorage.getItem("darkMode");
-  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const savedMode: string | null = localStorage.getItem("darkMode");
+  const prefersDark: boolean = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-  isDark.value = savedMode ? JSON.parse(savedMode) : prefersDark;
+  isDark.value = savedMode ? JSON.parse(savedMode) as boolean : prefersDark;
   updateTheme();
 });
 
-const toggleDarkMode = () => {
+const toggleDarkMode = (): void => {
   isDark.value = !isDark.value;
   updateTheme();
-  localStorage.setItem("darkMode", isDark.value);
+  localStorage.setItem("darkMode", JSON.stringify(isDark.value));
 };
 
-const updateTheme = () => {
+const updateTheme = (): void => {
   if (isDark.value) {
     document.documentElement.classList.add("dark");
   } else {
