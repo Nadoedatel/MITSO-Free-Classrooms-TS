7c8d613d599270acedd830fee3dd852c2a02306f
Nikita <nikita.vorontsov.1989@mail.ru>
2025-05-11 19:52:01 +0300

    Добавил плагин VuePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
@@ -11,6 +12,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    VitePWA({ registerType: 'autoUpdate' })
   ],
   resolve: {
     alias: {
